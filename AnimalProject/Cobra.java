8f079c276d04acd8cc0a17f0078f9d514d595647
Ryan <89214160+rkimbers@users.noreply.github.com>
2022-03-02 14:20:33 -0600

    Create Cobra.java

---
+++ AnimalProject/Cobra.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Coba here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Cobra extends Snake
+{
+    private boolean poison;
+    
+    public Cobra(){
+        super(4);
+        poison = true;
+    }
+}
