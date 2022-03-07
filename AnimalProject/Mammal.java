2370f40d99e2cf8facf6d7a5c1101421ad556b96
Ryan <89214160+rkimbers@users.noreply.github.com>
2022-03-07 14:11:52 -0600

    Mammal

---
+++ AnimalProject/Mammal.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Mammal here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public abstract class Mammal extends Animal
+{
+    protected boolean warmBlooded = true;
+    protected boolean hasFur;
+    protected boolean laysEggs;
+}
